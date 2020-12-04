480cc788ded403e3173b24be2b4b65c9fd5f2cb1
Jacob  Moura <jacobmoura@iMac-Pro.local>
2020-12-04 20:29:59 -0300

    .

--- lib/builders.dart
+++ lib/builders.dart
@@ -4,7 +4,7 @@ export 'src/consumer.dart';
 export 'src/selector.dart';
 export 'src/bloc_consumer.dart';
 
-typedef ProviderReturn<T> = T Function<T>();
+typedef ProviderReturn<T> = T? Function<T>();
 
 class Builders {
   static ProviderReturn? si;
